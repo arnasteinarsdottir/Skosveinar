b98380deac002b55e7cce9204486f3909abaa213
sahararos <sahararosin@gmail.com>
2025-11-21 13:33:14 +0000

    adding tailwind and router

---
+++ src/Components/testing.tsx
@@ -0,0 +1,13 @@
+
+
+function Testing() {
+  return (
+    <>
+      <div className="bg-blue-500">
+          <p className="text-red-400">Testing</p>
+      </div>
+    </>
+  )
+}
+
+export default Testing
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import "./main.css"
+import { BrowserRouter, Routes, Route } from "react-router";
+import Testing from "./Components/testing.tsx"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/testing" element={<Testing />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )

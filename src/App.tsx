89830b448cea59a811f9817b1fc7b84ca15e4238
qajqaasulimqaa <kaja.sulima@gmail.com>
2025-11-21 14:43:43 +0000

    Pushed navbar, burger menu, utils, tailwind, and fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 
+import Navbar from "./Components/Navbar.tsx";
+
 
 function App() {
   return (
     <>
+        <Navbar/>
       <div className="bg-blue-500">
           <p className="text-red-400">Halló</p>
       </div>

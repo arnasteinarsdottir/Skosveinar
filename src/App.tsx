5644ce04054d81ee1a9747762299bb00cfb1b4fb
Olibjarki <olibjarkig@gmail.com>
2025-12-01 13:13:43 +0000

    finished carousel

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
       <div className="flex justify-center items-center">
         <Carousel/>
       </div>
-        
+      
     </>
   )
 }
--- src/carousel.tsx
+++ src/carousel.tsx
@@ -13,7 +13,7 @@ const images = [
     'Images/21Gluggagægir.png',
     'Images/22Gáttaþefur.png',
     'Images/23Ketkrókur.png',
-    'Images/24Kertasníkir.png',
+    'Images/24Kertasníkir.png'
 ]
 
 const santas =[
@@ -29,12 +29,27 @@ const santas =[
     "Gluggagægir",
     "Gáttaþefur",
     "Ketkrókur",
-    "Kertasníkir",
+    "Kertasníkir"
+]
+
+const dates =[
+    "12. des",
+    "13. des",
+    "14. des",
+    "15. des",
+    "16. des",
+    "17. des",
+    "18. des",
+    "19. des",
+    "20. des",
+    "21. des",
+    "22. des",
+    "23. des",
+    "24. des"
 ]
 
 const Carousel = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
-    //const [santaName, setSantaName] = useState(santas[0]);
     const totalImages = images.length;
 
     const [isMovingLeft, setIsMovingLeft] = useState(false);
@@ -47,15 +62,15 @@ const Carousel = () => {
             setCurrentIndex((prevIndex) => (prevIndex === 0 ? totalImages - 1 : prevIndex - 1));
         }, 400);
     };
+    
     const goToNext = () => {
         setIsMovingLeft(true);
         setTimeout(() => {
             setIsMovingLeft(false);
             setCurrentIndex((prevIndex) => (prevIndex === totalImages - 1 ? 0 : prevIndex + 1));
         }, 400);
-        //setCurrentIndex((prevIndex) => (prevIndex === totalImages - 1 ? 0 : prevIndex + 1));
     }
- // Helper to get image index with wrapping
+    
     const getImageIndex = (offset: number) => {
         const index = currentIndex + offset;
         if (index < 0) return index + totalImages;
@@ -63,84 +78,113 @@ const Carousel = () => {
         return index;
     };
 
-    console.log((currentIndex +2) % totalImages)
     return (
-        <div>
-            <div className='grid grid-rows-8 grid-cols-7 gap-2 mt-8 w-[672px] overflow-x-hidden overflow-y-visible pt-8'>
+        <div className='min-w-[356px] mt-12'>
+            <div className='flex items-start gap-2 w-[356px] md:w-[672px] pt-8 overflow-hidden'>
                 
                 {/* Outside left */}
-                <img 
-                    src={images[getImageIndex(-3)]}
-                    alt={`Jólasveinn ${currentIndex + 1}`}
-                    className={`row-span-7 w-[80px] h-[80px] brightness-60 ml-[-88px] ${isMovingLeft && "transition-all duration-400 ease-in-out ml-[-236px]"} ${isMovingRight && "transition-all duration-400 ease-in-out ml-[0px]"}`}
-                />
+                <div className={`inline-block ml-[-176px] md:ml-[-88px] ${isMovingLeft && "transition-all duration-400 ease-in-out ml-[-280px] md:ml-[-176px]"} ${isMovingRight && "transition-all duration-400 ease-in-out md:ml-[0px]"}`}>
+                    <img 
+                        src={images[getImageIndex(-3)]}
+                        alt={santas[getImageIndex(-3)]}
+                        className="w-[80px] brightness-60"
+                    />
+                    <p className='w-[80px] font-[quicksand] text-[10px] font-semibold text-center'>
+                        {santas[getImageIndex(-3)]}
+                    </p>
+                </div>
 
                 {/* Small left */}
+                <div className={`inline-block ${isMovingRight && "transition-all duration-400 ease-in-out scale-125 md:scale-175 origin-top-left ml-[88px] md:ml-[0px]"}`}>
                     <img 
                         src={images[getImageIndex(-2)]}
-                        alt={`Jólasveinn ${currentIndex + 1}`}
-                        className={`row-span-7 w-[80px] h-[80px] brightness-60 ${isMovingRight && "transition-all duration-400 ease-in-out brightness-80 scale-175 origin-top-left"}`}
+                        alt={santas[getImageIndex(-2)]}
+                        className={`w-[80px] brightness-60 ${isMovingRight && "transition-all duration-400 ease-in-out brightness-[80%]"}`}
                     />
+                    <p className='w-[80px] font-[quicksand] text-[10px] font-semibold text-center'>
+                        {santas[getImageIndex(-2)]}
+                    </p>
+                </div>
 
                 {/* Medium left */}
+                <div className={`inline-block ${isMovingLeft && "transition-all duration-400 ease-in-out scale-[57%] md:scale-[57%] origin-top-left ml-[-4px] md:ml-[0px]"} ${isMovingRight && "transition-all duration-400 ease-in-out scale-140 md:scale-143 origin-top-left ml-[20px] md:ml-[60px]"}`}>
                     <img 
                         src={images[getImageIndex(-1)]}
-                        alt={`Jólasveinn ${currentIndex + 1}`}
-                        className={`row-span-7 w-[140px] h-[140px] brightness-80 ${isMovingLeft && "transition-all duration-400 ease-in-out brightness-[60%] scale-57 origin-top-right"} ${isMovingRight && "transition-all duration-400 ease-in-out ml-[60px] brightness-100 scale-143 origin-top-left"}`}
+                        alt={santas[getImageIndex(-1)]}
+                        className={`w-[100px] md:w-[140px] brightness-80 ${isMovingLeft && "transition-all duration-400 ease-in-out brightness-[60%]"} ${isMovingRight && "transition-all duration-400 ease-in-out brightness-[100%]"}`}
                     />
+                    <p className={`w-[100px] md:w-[140px] font-[quicksand] text-[12.5px] md:text-[17.5px] font-semibold text-center`}>
+                        {santas[getImageIndex(-1)]}
+                    </p>
+                </div>
 
-                {/* Large center */}
-                
-                    <img 
-                        src={images[currentIndex]}
-                        alt={`Jólasveinn ${currentIndex + 1}`} 
-                        className={`row-span-7 w-[200px] h-[200px] ${isMovingLeft && "transition-all duration-400 ease-in-out brightness-80 scale-70 origin-top-left"} ${isMovingRight && "transition-all duration-400 ease-in-out brightness-80 scale-70 origin-top-right"}`}
-                    />
-                    
-                    
+                {/* Large center*/}
+                <div className={`inline-block ${isMovingLeft && "transition-all duration-400 ease-in-out scale-[71.5%] md:scale-[70%] origin-top-left ml-[px] md:ml-[-60px]"} ${isMovingRight && "transition-all duration-400 ease-in-out scale-[71.5%] md:scale-[70%] origin-top-right"}`}>
+                    <a href="#">
+                        <img 
+                            src={images[currentIndex]}
+                            alt={santas[currentIndex]} 
+                            className={`w-[140px] md:w-[200px] ${isMovingLeft && "transition-all duration-400 ease-in-out brightness-[80%]"} ${isMovingRight && "transition-all duration-400 ease-in-out brightness-[80%]"}`}
+                        />
+                        <p className={`w-[140px] md:w-[200px] font-[quicksand] text-[17.5px] md:text-[25px] font-semibold text-center`}>
+                            {santas[currentIndex]}
+                        </p>
+                    </a>
+                </div>
 
-                {/* Medium right */}
+                {/* Medium right*/}
+                <div className={`inline-block ${isMovingLeft && "transition-all duration-400 ease-in-out scale-140 md:scale-143 origin-top ml-[-20px] md:ml-[-30px]"} ${isMovingRight && "transition-all duration-400 ease-in-out scale-57 origin-top-right ml-[-30px] md:ml-[-60px]"}`}>
                     <img 
                         src={images[getImageIndex(+1)]}
-                        alt={`Jólasveinn ${currentIndex + 1}`}
-                        className={`row-span-7 w-[140px] h-[140px] brightness-80 ${isMovingLeft && "transition-all duration-400 ease-in-out brightness-100 scale-143 origin-top-right"} ${isMovingRight && "transition-all duration-400 ease-in-out brightness-[60%] scale-57 origin-top-left"}`}
+                        alt={santas[getImageIndex(+1)]}
+                        className={`w-[100px] md:w-[140px] brightness-80 ${isMovingLeft && "transition-all duration-400 ease-in-out brightness-[100%]"} ${isMovingRight && "transition-all duration-400 ease-in-out brightness-[60%]"}`}
                     />
+                    <p className='w-[100px] md:w-[140px] font-[quicksand] text-[12.5px] md:text-[17.5px] font-semibold text-center'>
+                        {santas[getImageIndex(+1)]}
+                    </p>
+                </div>
 
-                {/* Small right */}
+                {/* Small right*/}
+                <div className={`inline-block ${isMovingLeft && "transition-all duration-400 ease-in-out scale-125 md:scale-175 origin-top ml-[30px] md:ml-[60px]"}`}>
                     <img 
                         src={images[getImageIndex(+2)]}
-                        alt={`Jólasveinn ${currentIndex + 1}`} 
-                        className={`row-span-7 w-[80px] h-[80px] brightness-60 ${isMovingLeft && "transition-all duration-400 ease-in-out brightness-80 scale-175 origin-top-left"}`}
+                        alt={santas[getImageIndex(+2)]} 
+                        className={`w-[80px] brightness-60 ${isMovingLeft && "transition-all duration-400 ease-in-out brightness-[80%]"}`}
                     />
+                    <p className='w-[80px] font-[quicksand] text-[10px] font-semibold text-center'>
+                        {santas[getImageIndex(+2)]}
+                    </p>
+                </div>
 
-                {/* Outside right */}
-                    <img 
-                        src={images[getImageIndex(+3)]}
-                        alt={`Jólasveinn ${currentIndex + 1}`}
-                        className='row-span-7 w-[80px] h-[80px] ml-[60px] brightness-60'
-                    />
-                <p>Stekkjastaur</p>
-                <p>Stekkjastaur</p>
-                <p>Stekkjastaur</p>
-                <p>Stekkjastaur</p>
-                <p>Stekkjastaur</p>
-                <p>Stekkjastaur</p>
-                <p>Stekkjastaur</p>
+                {/* Outside right*/}
+                <div className={`inline-block ${isMovingLeft && "transition-all duration-400 ease-in-out ml-[30px]"} ${isMovingRight && "transition-all duration-400 ease-in-out mr-[88px]"}`}>
+                        <img 
+                            src={images[getImageIndex(+3)]}
+                            alt={santas[getImageIndex(+3)]}
+                            className='w-[80px] brightness-60'
+                        />
+                        <p className='w-[80px] font-[quicksand] text-[10px] font-semibold text-center'>
+                            {santas[getImageIndex(+3)]}
+                        </p>
+                </div>
             </div>
-            <div className='flex justify-center gap-20 mb-4'>
+            <div className='flex justify-between items-center w-full px-4 md:px-[186px]'>
                 <button 
                     onClick={goToPrevious}
                     disabled={isMovingRight}
-                    className='flex justify-center items-center bg-[#4E2513] w-[53px] h-7 mt-4 rounded-lg hover:cursor-pointer'>
+                    className='flex justify-center items-center bg-[#4E2513] w-[53px] h-7 rounded-lg hover:cursor-pointer'>
                     <img 
                         src="Images/ArrowLeft.svg" 
                         alt="Previous" 
                     />
                 </button>
+                <p className='font-[quicksand] text-[16px] md:text-[20px] font-semibold'>
+                    {dates[currentIndex]}
+                </p>
                 <button 
                     onClick={goToNext}
                     disabled={isMovingLeft}
-                    className='flex justify-center items-center bg-[#4E2513] w-[53px] h-7 mt-4 rounded-lg hover:cursor-pointer'>
+                    className='flex justify-center items-center bg-[#4E2513] w-[53px] h-7 rounded-lg hover:cursor-pointer'>
                     <img 
                         src="Images/ArrowRight.svg" 
                         alt="Next"

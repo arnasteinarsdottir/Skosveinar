8c483331c51c5dfc366912da109a87f4d5d83659
sahararos <sahararosin@gmail.com>
2025-12-01 12:13:36 +0000

    updated organiziing and profile page

--- src/Pages/ProfilePage.tsx
+++ src/Pages/ProfilePage.tsx
@@ -1,10 +1,21 @@
-<<<<<<< HEAD:src/ProfilePage.tsx
-import GiftOverviewBox from "./Components/GiftOverviewBox";
-import TotalPriceCalculator from "./Components/TotalPriceCalculator";
-import FancyDropdown from "./Components/FancyDropdown";
-import BrownFilledButton from "./Components/BrownFilledButton";
-import AddChildModal from "./Components/AddChildModal";
+import GiftOverviewBox from "../Components/GiftOverviewBox";
+import TotalPriceCalculator from "../Components/TotalPriceCalculator";
+import FancyDropdown from "../Components/FancyDropdown";
+import BrownFilledButton from "../Components/BrownFilledButton";
+import AddChildModal from "../Components/AddChildModal";
 import {useState} from "react"; 
+
+type Gift = {
+    [key: string]: {
+        ideas: string;
+        price: number;
+    };
+}
+
+type Child = {
+    name: string;
+    gifts: Gift;
+}
 
 
 function ProfilePage() {
@@ -17,27 +28,16 @@ function ProfilePage() {
           setNameSelected(name)
       }
 
-      const childrenSTR = localStorage.getItem("children");
-      const options = JSON.parse(childrenSTR)?.map((child) => {
+      const childrenSTR = localStorage.getItem("children") || "[]";
+      const options = JSON.parse(childrenSTR)?.map((child: Child) => {
         return (
             child.name
         )
       });
-
-=======
-import GiftOverviewBox from "../Components/GiftOverviewBox";
-import TotalPriceCalculator from "../Components/TotalPriceCalculator";
-import FancyDropdown from "../Components/FancyDropdown";
-import AddChildModal from "../Components/AddChildModal";
-import Navbar from "@/Components/Navbar";
-
-
-function ProfilePage() {
->>>>>>> c951ab96059f81f77e52134613a029a42ca07057:src/Pages/ProfilePage.tsx
-     const GetFromLocalStorage = () => {
-      let trueData: any[] = []
+     const GetFromLocalStorage = (): Gift => {
+      let trueData: Child[] = []
       const storedJsonString = childrenSTR
-      if (!storedJsonString) return trueData
+      if (!storedJsonString) return trueData [0].gifts
 
       try {
         const retrievedArrayOfObjects = JSON.parse(storedJsonString)
@@ -58,7 +58,6 @@ function ProfilePage() {
   const dateOptions = ["12. des", "13. des", "14. des", "15. des", "16. des", "17. des", "18. des", "19. des", "20. des", "21. des", "22. des", "23. des", "24. des"]
 
   const selectedChildGifts = GetFromLocalStorage()
-  console.log(selectedChildGifts);
   const CalculateTotalPrice = () => {
     let Total = 0;
     for (let i = 0; i < dateOptions.length; i++){
@@ -72,24 +71,17 @@ function ProfilePage() {
   return (
     <>
       <div className=" flex flex-col items-center">
-<<<<<<< HEAD:src/ProfilePage.tsx
         <div className="flex justify-between items-start w-98 mt-14">
           <FancyDropdown selectOption = {selectChild} optionSelected = {nameSelected} options = {options} />
           <BrownFilledButton 
             onClick = {() => setIsModalOpen(true)}
           />
-=======
-        <Navbar />
-      <div className="pt-20"></div>
-        <div className="flex justify-between items-center w-98 mt-14">
-          <FancyDropdown />
->>>>>>> c951ab96059f81f77e52134613a029a42ca07057:src/Pages/ProfilePage.tsx
         </div>
         <div className="w-98 flex justify-end pt-12 md:flex md:justify-center">
           <TotalPriceCalculator TotalPrice={CalculateTotalPrice()}/>
         </div>
         <div className="py-6 grid grid-cols-1 md:grid-cols-2 gap-6">
-          {dateOptions.map((date, key) => {
+          {dateOptions.map((date) => {
             return (
               <div key={date}>
                 <GiftOverviewBox date={date} gift={GetFromLocalStorage()[date]?.ideas} price={GetFromLocalStorage()[date]?.price} />

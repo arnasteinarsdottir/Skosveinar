3fdddd35a4eb9ffd89903555ad0d88acc9fbc7ea
qajqaasulimqaa <kaja.sulima@gmail.com>
2025-12-02 13:23:43 +0000

    new route

--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,37 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./App.tsx";
 import "./main.css";
+
 import Search from "./Pages/Search.tsx";
 import WishList from "./Pages/WishList.tsx";
 import ProfilePage from "./Pages/ProfilePage.tsx";
 import Organize from "./Pages/Organize.tsx";
-import LoginPage from './Pages/Login.tsx';
-import AboutTradition from './Pages/AboutTradition.tsx';
-import Testing from './Pages/Testing.tsx';
+import LoginPage from "./Pages/Login.tsx";
+import AboutTradition from "./Pages/AboutTradition.tsx";
+import Testing from "./Pages/Testing.tsx";
 
 import { HashRouter, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HashRouter>
       <Routes>
+        {/* homepage */}
         <Route path="/" element={<App />} />
-        <Route path="/Search" element={<Search />} />
+
+        {/* main pages */}
+        <Route path="/search" element={<Search />} />
         <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/AboutTradition" element={<AboutTradition />} />
+        <Route path="/abouttradition" element={<AboutTradition />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/profilepage" element={<ProfilePage />} />
         <Route path="/organize" element={<Organize />} />
         <Route path="/wishlist" element={<WishList />} />
+
+        {/* dev/testing page */}
         <Route path="/testing" element={<Testing />} />
       </Routes>
     </HashRouter>
-  </StrictMode>,
+  </StrictMode>
 );

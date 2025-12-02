86b11d617710c7f4eb54e228b25d5aa8a8876ca7
qajqaasulimqaa <kaja.sulima@gmail.com>
2025-12-02 13:30:32 +0000

    Old router

--- src/main.tsx
+++ src/main.tsx
@@ -12,26 +12,22 @@ import LoginPage from "./Pages/Login.tsx";
 import AboutTradition from "./Pages/AboutTradition.tsx";
 import Testing from "./Pages/Testing.tsx";
 
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
-        {/* homepage */}
         <Route path="/" element={<App />} />
-
-        {/* main pages */}
-        <Route path="/search" element={<Search />} />
+        <Route path="/Search" element={<Search />} />
         <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/abouttradition" element={<AboutTradition />} />
+        <Route path="/AboutTradition" element={<AboutTradition />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/profilepage" element={<ProfilePage />} />
         <Route path="/organize" element={<Organize />} />
         <Route path="/wishlist" element={<WishList />} />
-
-        {/* dev/testing page */}
         <Route path="/testing" element={<Testing />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
